0f5e71a4e7cecb7f42f54759770ec59bf27b63a8
dennislee928 <pcleegood@gmail.com>
2024-11-24 14:06:35 +0800

    UPDATE

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    ppr: true,
-  },
-};
-
 module.exports = {
   env: {
     CLIMATIQ_API_KEY: process.env.CLIMATIQ_API_KEY,

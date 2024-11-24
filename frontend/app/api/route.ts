9f817eab34ba98d59ac5c68b78b748abcf315b11
dennislee928 <pcleegood@gmail.com>
2024-11-24 15:33:22 +0800

    add bcryptjs

--- frontend/app/api/route.ts
+++ frontend/app/api/route.ts
@@ -1,6 +1,8 @@
-// app/api/auth/route.ts
+// app/api/route.ts
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+
+export const runtime = "nodejs"; // Add this line to use Node.js runtime
 
 export async function POST(request: Request) {
   const { password } = await request.json();

f9e6aa021b04c4b8b44b0f725425093c1d5badd7
dennislee928 <pcleegood@gmail.com>
2024-11-25 16:29:42 +0800

    fix: add gin lib for router

--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,24 @@ import (
 	"carbon-rights-backend/routes"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin" // If using Gin
 )
 
 func main() {
+    // Initialize Router using gin
+    router := gin.Default()
     // Initialize application configuration (database, environment variables, etc.)
     db := config.InitializeDB()
     defer db.Close() // Close the database connection when the app exits
 
     // Set up routes using Gorilla Mux
     routes.SetupRoutes()
+     // Setuproutes
+     router.GET("/", func(c *gin.Context) {
+        // handler logic
+    })
+    router.Run(":8080")  
 
     // Start the HTTP server
     log.Println("Server is starting on port 8080...")

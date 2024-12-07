4f61db9d143f5ac98656e4e5b28a6a51f053f72e
dennislee928 <pcleegood@gmail.com>
2024-12-07 11:36:04 +0800

    Update header.jsx

--- Dev-frontend/carbon-trading/header.jsx
+++ Dev-frontend/carbon-trading/header.jsx
@@ -1,7 +1,24 @@
 export default function HomePage() {
   return (
     <Head>
-      <link rel="canonical" href="https://yourwebsite.com/page-url" />
+      <link
+        rel="canonical"
+        href="https://www.para-universe-energy-exchange-station.com/"
+      />
+      <meta
+        property="og:title"
+        content="para-universe-energy-exchange-station.com"
+      />
+      <meta property="og:description" content="CF LAB Twister5.com.tw" />
+      <meta
+        property="og:image"
+        content="https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg"
+      />
+      <meta
+        property="og:url"
+        content="https://www.para-universe-energy-exchange-station.com/"
+      />
+      <meta property="og:type" content="website" />
     </Head>
   );
 }
